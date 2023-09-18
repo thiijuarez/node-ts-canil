3e1bae814e2bd3725b8795c277d3c0744dc1219b
thiijuarez <juarez.thiagopro@gmail.com>
2023-09-18 11:18:17 -0300

    trabalhando nos views e controllers

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,40 @@
 import { Request, Response } from "express";
 
+import { createMenuObject } from '../helpers/createMenuObject';
+
 export const home = (req: Request, res: Response) => {
-    res.send('Home no Controller');
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 };
 export const dogs = (req: Request, res: Response) => {
-
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cães',
+            background: 'banner_dog.jpg'
+        }
+    });
 };
 export const cats = (req: Request, res: Response) => {
-
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 };
 export const fishes = (req: Request, res: Response) => {
-
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 };
